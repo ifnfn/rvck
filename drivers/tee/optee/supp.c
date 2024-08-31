ead030c276ec53b3734d6f5167bb548b829a6f97
Han Gao <gaohan@iscas.ac.cn>
2024-09-01 01:59:49 +0800

    driver:tee:Allow to freeze when tee supplicant is freezed
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- drivers/tee/optee/supp.c
+++ drivers/tee/optee/supp.c
@@ -6,6 +6,7 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include "optee_private.h"
+#include <linux/freezer.h>
 
 struct optee_supp_req {
 	struct list_head link;
@@ -161,6 +162,7 @@ u32 optee_supp_thrd_req(struct tee_context *ctx, u32 func, size_t num_params,
 
 		mutex_unlock(&supp->mutex);
 	} else {
+		try_to_freeze();
 		ret = req->ret;
 	}
 
